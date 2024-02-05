be80a30f596fc27c2d93d4a6d70843ffc849107e
Alexandre <duffhd@outlook.com>
2024-02-05 20:12:53 -0300

    Enhance project CMake output and compile script for Windows

--- squirrel/compile.ps1
+++ squirrel/compile.ps1
@@ -2,7 +2,7 @@ mkdir build
 cd build
 cmake ..
 msbuild .\ALL_BUILD.vcxproj
+cd ..
+cd bin
 cp ..\example.nut .
-cp squirrel\bin\Debug\squirrel.dll .
-cp squirrel\bin\Debug\sqstdlib.dll .
-.\Debug\squirrel_embed.exe
+.\squirrel_embed.exe
