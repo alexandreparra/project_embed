6459f6205d86a2a6eda5252d591913cd1fc2c0b0
Alexandre <duffhd@outlook.com>
2024-02-08 21:28:58 -0300

    Add native function example and enhance descriptions

--- squirrel/main.cpp
+++ squirrel/main.cpp
@@ -7,6 +7,7 @@
 
 // -------------------------------------
 // Print functions
+
 void squirrel_print_error(HSQUIRRELVM vm, const SQChar* fmt, ...)
 {
     va_list vl;
@@ -35,14 +36,14 @@ void squirrel_triangle_area(HSQUIRRELVM vm, float base, float height)
     sq_pushstring(vm, _SC("triangle_area"), -1);
     if (SQ_SUCCEEDED(sq_get(vm, -2)))
     {
-        // We pass the parameters in, first the root table.
-        // Then we pass the parameters with the same sequence
+        // We pass the arguments in, first the root table.
+        // Then we pass the arguments with the same sequence
         // from what we defined in Squirrel.
         sq_pushroottable(vm);
         sq_pushfloat(vm, base);
         sq_pushfloat(vm, height);
-        // Pass SQTrue so that the returned value from the Squirrel function is pushed
-        // to the top of the stack.
+        // Pass SQTrue as the third argument so that the returned value from
+        // the Squirrel function is pushed to the top of the stack.
         sq_call(vm, 3, SQTrue, SQTrue);
 
         // Retrieve the returned value from the top of the stack.
@@ -55,10 +56,10 @@ void squirrel_triangle_area(HSQUIRRELVM vm, float base, float height)
     sq_settop(vm, top);
 }
 
-// An example function call with no parameters and no return value.
+// An example function call with no arguments and no return value.
 void squirrel_log(HSQUIRRELVM vm)
 {
-    // Save the top of the stack for later.
+    // Save the top of the stack to latter reset its state.
 	SQInteger top = sq_gettop(vm);
     sq_pushroottable(vm);
 
@@ -67,9 +68,9 @@ void squirrel_log(HSQUIRRELVM vm)
     sq_pushstring(vm, _SC("log"), -1);
     if (SQ_SUCCEEDED(sq_get(vm, -2)))
     {
-        // Now we can pass in the parameters, in this case, the Squirrel
+        // Now we can pass in the arguments, in this case, the Squirrel
         // function doesn't have any beside the root table.
-        // root table always accounts as one parameter.
+        // root table always accounts as one argument.
         sq_pushroottable(vm);
         sq_call(vm, 1, SQFalse, SQTrue);
     }
@@ -77,6 +78,69 @@ void squirrel_log(HSQUIRRELVM vm)
     // We restore the stack back to when we entered this function.
     // Basically we are manipulating the stack ourselves just like in assembly.
     sq_settop(vm, top);
+}
+
+// -------------------------------------
+// Native functions
+
+// Native function with 2 arguments and a return value.
+SQInteger calculate_triangle_area(HSQUIRRELVM vm)
+{
+    float base, height;
+
+    SQInteger arg_number = sq_gettop(vm);
+
+    // Here index 3 and 2 are our float parameters height and base.
+    if(sq_gettype(vm, arg_number) == OT_FLOAT && sq_gettype(vm, arg_number - 1) == OT_FLOAT) {
+        sq_getfloat(vm, arg_number, &height);
+        sq_getfloat(vm, arg_number - 1, &base);
+
+        float area = 0.5f * base * height;
+
+        // Push the value to the stack so that Squirrel can access it and return 1
+        // to indicate that we are indeed returning a value.
+        sq_pushfloat(vm, area);
+        return 1;
+    }
+
+    // If the number of arguments are wrong, indicate that we return no value.
+    return 0;
+}
+
+// Native function with 1 argument and no return value.
+SQInteger log_cpp_arg(HSQUIRRELVM vm)
+{
+    std::cout << "C++: Called native function 'log_cpp_arg' from Squirrel!\n";
+
+    // We expect only one argument from the Squirrel script, a string.
+    SQInteger arg_number = sq_gettop(vm);
+
+    // Our parameters are at the top of the stack, at index 1 is the root table which
+    // we don't care about, index 2 is our first and only parameter.
+    if (sq_gettype(vm, arg_number) == OT_STRING) {
+        const char *log_value;
+        sq_getstring(vm, arg_number, &log_value);
+        std::cout << "C++: Log value from Squirrel: " << log_value << '\n';
+    }
+
+    return 0;
+}
+
+// Native function with no arguments and no return value.
+SQInteger log_cpp(HSQUIRRELVM vm)
+{
+    std::cout << "C++: Called native function 'log_cpp' from Squirrel!\n";
+    return 0;
+}
+
+// Utility function
+void register_native_function(HSQUIRRELVM vm, const char *function_name, SQFUNCTION fn)
+{
+    sq_pushroottable(vm);
+    sq_pushstring(vm, function_name, -1);
+    sq_newclosure(vm, fn, 0);
+    sq_newslot(vm, -3, SQFalse);
+    sq_pop(vm, 1);
 }
 
 int main()
@@ -93,7 +157,14 @@ int main()
     sq_setprintfunc(vm, squirrel_print, squirrel_print_error);
     sq_pushroottable(vm);
 
-    // sqstd_dofile is an utility function from the Squirrel standard library that helps us compile
+    // Here we are defining a native functions which are C/C++ function that can
+    // be called from inside Squirrel. Look at the conventions of a native function
+    // at: http://squirrel-lang.org/squirreldoc/reference/embedding/creating_a_c_function.html
+    register_native_function(vm, "log_cpp", &log_cpp);
+    register_native_function(vm, "log_cpp_arg", &log_cpp_arg);
+    register_native_function(vm, "calculate_triangle_area", &calculate_triangle_area);
+
+    // sqstd_dofile is a utility function from the Squirrel standard library that helps us compile
     // our .nut files into bytecode. If you want to do it by hand, you can follow the example from the
     // Squirrel reference: http://squirrel-lang.org/squirreldoc/reference/embedding/compiling_a_script.html
     if (SQ_SUCCEEDED(sqstd_dofile(vm, _SC("example.nut"), SQFalse, SQTrue)))
@@ -102,6 +173,7 @@ int main()
         squirrel_triangle_area(vm, 10, 5);
     }
 
+    // Cleanup
     sq_pop(vm, 1); // pops the root table
     sq_close(vm);
 
